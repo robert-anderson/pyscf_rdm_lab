96cac35a0220d66ebc3eec9d742816221e6fe5cc
Thomas Young Cluster RITS Do NOT reset password <mmm0043@node-k98t-032.thomas.ucl.ac.uk>
2018-01-23 02:01:57 +0000

    explicit RDMs working

--- rdm_factory.py
+++ rdm_factory.py
@@ -1,8 +1,8 @@
 from pyscf.fci.addons import cre_a, cre_b, des_a, des_b
+from pyscf.fci import rdm as pyscf_rdm
 import numpy as np
 import itertools
-#import tensor_utils
-
+import tensor_utils
 
 def explicit_parity(tup):
     # lazy implementation to test faster ones
@@ -20,34 +20,37 @@ def explicit_parity(tup):
                     t+=len(tup)-1
         if work==range(len(tup)):
             break
-    return t%2
+    return 1-2*(t%2)
 
-
-class ParityTranspose:
-    def __init__(self, rank):
-        self.rank = rank
-        self.iterable = itertools.permutations(range(rank), rank)
-    def next(self):
-        tmp = self.iterable.next()
-       # tot = 0
-       # t = -1
-       # for i in range(self.rank):
-       #     t*=-1
-       #     tot+=t*(tmp[i]-i)
-
-        return tmp, tot
-
-
-p = ParityTranspose(5)
-
-while True:
-    print p.next()
-
-assert(0)
+def make_all_perms(rank):
+	all_perms = []
+	for order in itertools.permutations(range(rank), rank):
+		all_perms.append([order, explicit_parity(order)])
+	return all_perms
 
 def apply_spin_resolved_rdm_symmetries(rdm):
-    rank = len(rdm.shape)/2
+	# iterate over upper triangle
     nbasis = rdm.shape[0]
+    rank = len(rdm.shape)/2
+    all_perms = make_all_perms(rank)
+    for hole_inds in itertools.combinations(range(nbasis), rank):
+        for elec_inds in itertools.combinations(range(nbasis), rank):
+            for hole_perm in all_perms:
+                for elec_perm in all_perms:
+                    rdm[tuple(hole_inds[i] for i in hole_perm[0])+tuple(elec_inds[i] for i in elec_perm[0])] = \
+                    rdm[hole_inds+elec_inds]*hole_perm[1]*elec_perm[1]
+
+def spin_trace(sr_rdm):
+	nbasis = sr_rdm.shape[0]
+	rank = len(sr_rdm.shape)/2
+	sf_rdm = np.zeros((nbasis/2,)*rank*2)
+	for inds in itertools.product(range(nbasis/2), repeat=rank*2):
+		for spin_sig in itertools.product((0,1), repeat=rank):
+			sf_rdm[inds]+=sr_rdm[tuple(2*inds[i]+spin_sig[i] for i in range(rank))+tuple(2*inds[rank+i]+spin_sig[i] for i in range(rank))]
+	return sf_rdm
+
+
+
 
 '''
 cre_a(ci0, norb, neleca_nelecb, ap_id)
@@ -131,32 +134,47 @@ def split_spin_inds(inds):
 
 def make_no_spin_resolved_rdm(civec, norbs, nelecs, rank):
     rdm = np.zeros((2*norbs,)*(2*rank))
-    for inds in itertools.product(range(2*norbs), repeat=2*rank):
-        spat_inds, alphas = split_spin_inds(inds)
-        rdm[inds] = calc_no_spin_resolved_element(civec, norbs, nelecs,
-                tuple(spat_inds[2*i] for i in range(rank)),
-                tuple(spat_inds[2*i+1] for i in range(rank)),
-                tuple(alphas[2*i] for i in range(rank)),
-                tuple(alphas[2*i+1] for i in range(rank)))
+	# only fill upper triangle
+    for hole_inds in itertools.combinations(range(2*norbs), rank):
+        spat_hole_inds, hole_alphas = split_spin_inds(hole_inds)
+    	for elec_inds in itertools.combinations(range(2*norbs), rank):
+            spat_elec_inds, elec_alphas = split_spin_inds(elec_inds)
+            if sum(elec_alphas)!=sum(hole_alphas):
+                continue
+            rdm[hole_inds+elec_inds] = calc_no_spin_resolved_element(civec, norbs, nelecs,
+                spat_hole_inds, spat_elec_inds, hole_alphas, elec_alphas)
+    apply_spin_resolved_rdm_symmetries(rdm)
     return rdm
-
-#def make_no_spin_resolved_rdm_sparse(civec, norbs, nelecs, rank):
-
-
-
-
-def spin_trace_no(rdm):
-    rank = len(rdm.shape)/2
-    norbs = rdm.shape[0]/2
-    sf_rdm = np.zeros((norbs,)*(2*rank))
-    for inds in itertools.product(range(2*norbs), repeat=2*rank):
-        sf_rdm[split_spin_inds(inds)[0]]+=rdm[inds]
-    return sf_inds
 
 
 civec=np.load('ci.npy')
-rdm=make_pose_spinfree_rdm(civec, 6, (3,3), 2)
-dm2 = np.load('dm2.npy')
-print np.allclose(rdm, dm2)
-print tensor_utils.allclose_transpose(rdm, dm2)
+no_sr_rdm1=make_no_spin_resolved_rdm(civec, 6, (3,3), 1)
+no_sr_rdm2=make_no_spin_resolved_rdm(civec, 6, (3,3), 2)
+no_sr_rdm3=make_no_spin_resolved_rdm(civec, 6, (3,3), 3)
+
+no_sf_rdm1=spin_trace(no_sr_rdm1)
+no_sf_rdm2=spin_trace(no_sr_rdm2)
+no_sf_rdm3=spin_trace(no_sr_rdm3)
+
+pyscf_pose_sf_rdm1, pyscf_pose_sf_rdm2, pyscf_pose_sf_rdm3 = pyscf_rdm.make_dm123('FCI3pdm_kern_sf', civec, civec, 6, (3,3))
+pyscf_no_sf_rdm1, pyscf_no_sf_rdm2, pyscf_no_sf_rdm3 = pyscf_rdm.reorder_dm123(pyscf_pose_sf_rdm1, pyscf_pose_sf_rdm2, pyscf_pose_sf_rdm3)
+
+print tensor_utils.allclose_transpose(pyscf_no_sf_rdm2, no_sf_rdm2)
+print tensor_utils.allclose_transpose(pyscf_no_sf_rdm3, no_sf_rdm3)
+
+#print rdm2[0,0,0,0]
+#print rdm2[0,1,0,1]
+
+
+
+#rdm=make_pose_spinfree_rdm(civec, 6, (3,3), 2)
+#dm2 = np.load('dm2.npy')
+#print np.allclose(rdm, dm2)
+#print tensor_utils.allclose_transpose(rdm, dm2)
+
+
+
+
+
+
 
